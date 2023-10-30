aa5cf2f60c94c40ca563c6e5224f8d19b38aa383
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-10-30 15:57:15 +0100

    Add new functionalities

--- SmartHomeBackend/Controllers/LightsController.cs
+++ SmartHomeBackend/Controllers/LightsController.cs
@@ -35,5 +35,34 @@ namespace SmartHomeBackend.Controllers
                 }
             }
         }
+
+        [Route("{lightId}/log")]
+        [HttpGet]
+        public async Task<IActionResult> GetLightState(int systemId, int boardId, int lightId)
+        {
+            string url = $"http://127.0.0.1:5000/api/system/{systemId}/board/{boardId}/devices/lights/{lightId}/log";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    var jsonDocument = JsonDocument.Parse(jsonResponse);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return Ok(jsonDocument);
+                    }
+                    else
+                    {
+                        return StatusCode(int.Parse(response.StatusCode.ToString()), $"An error occurred: {response.Content}");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    return StatusCode(500, $"An error occurred: {e.Message}");
+                }
+            }
+        }
     }
 }
