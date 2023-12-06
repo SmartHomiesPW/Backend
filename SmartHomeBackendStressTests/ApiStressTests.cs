a3f7e7eb7955fa20ecbe3880daff7fb01502a2eb
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-12-06 15:04:38 +0100

    Add stress tests project

---
+++ SmartHomeBackendStressTests/ApiStressTests.cs
@@ -0,0 +1,31 @@
+using SmartHomeBackend.Services;
+using System.Diagnostics;
+using Xunit;
+
+namespace SmartHomeBackendStressTests
+{
+    public class ApiStressTests
+    {
+        private const string BaseUrl = "http://127.0.0.1:5000/api/system/1/board/1/devices/lights/1";
+        private const int NumberOfRequests = 100;
+
+        [Fact]
+        public async Task TestApiStress()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            DeviceService ds = new DeviceService();
+            
+            var tasks = new Task[NumberOfRequests];
+            for (var i = 0; i < NumberOfRequests; i++)
+            {
+                tasks[i] = ds.SendHttpRequest(BaseUrl);
+            }
+            await Task.WhenAll(tasks);
+
+            stopwatch.Stop();
+            Assert.True(stopwatch.ElapsedMilliseconds <= 1000);
+            
+        }
+    }
+}
