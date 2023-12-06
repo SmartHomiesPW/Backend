a96904f368dc470f402067a8eb9144ed9da03913
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-12-06 04:50:34 +0100

    Make first successful unit test

--- SmartHomeBackend/Database/SmartHomeDbContext.cs
+++ SmartHomeBackend/Database/SmartHomeDbContext.cs
@@ -6,10 +6,12 @@ namespace SmartHomeBackend.Database
 {
     public class SmartHomeDbContext : DbContext
     {
+
+        public SmartHomeDbContext(){ }
         public SmartHomeDbContext(DbContextOptions<SmartHomeDbContext> options) : base(options) { }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Models.System> Systems { get; set; }
-        public DbSet<SwitchableLight> SwitchableLights { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Models.System> Systems { get; set; }
+        public virtual DbSet<SwitchableLight> SwitchableLights { get; set; }
     }
 }
